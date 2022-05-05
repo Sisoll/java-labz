ffc6b6e2cab14001d41b49ca7dfea529cb5b2b61
Sisol Shie <sisolshie@gmail.com>
2022-05-05 13:47:40 +0800

    fix 題意未弄清楚
    多個shape欄位,在進去使用if去判斷符合題目要求

--- homework/ShapeCalculatorV2.java
+++ homework/ShapeCalculatorV2.java
@@ -2,7 +2,7 @@ package homework;
 
 /**
  * 
- * - 新建一個類別 ShapeCalculatorV2 - 兩個方法 getPerimeter 及 getArea - 建構 ShapeCalculator
+ * 新建一個類別 ShapeCalculatorV2 - 兩個方法 getPerimeter 及 getArea - 建構 ShapeCalculator
  * 時可以傳入 Rectangle or Circle - 當呼叫 getPerimeter or getArea 時，能給我對應形狀的周長 or 面積
  * 
  * @author SisolShie
@@ -12,61 +12,58 @@ public class ShapeCalculatorV2 {
 
 	public static void main(String[] args) {
 
-		ShapeCalculatorV2 r = new ShapeCalculatorV2(10, 12);
+		Rectangle rectangle = new Rectangle();
+		rectangle.width = 5;
+		rectangle.length = 8;
 
-		r.getArea(r.radius);
-		r.getArea(r.x, r.y);
+		Circle circle = new Circle();
+		circle.radius = 10;
 
-		r.getPerimeter(r.radius);
-		r.getPerimeter(r.x, r.y);
+		ShapeCalculatorV2 r = new ShapeCalculatorV2(rectangle);
+		ShapeCalculatorV2 t = new ShapeCalculatorV2(circle);
+
+		r.getArea();
+		t.getPerimeter();
 
 	}
 
 	int radius;
-	int x, y;
+	int length;
+	int width;
+	String shape;
 
 	public ShapeCalculatorV2() {
 	}
 
-	public ShapeCalculatorV2(int radius) {
-		this.radius = radius;
+	public ShapeCalculatorV2(Circle circle) {
+		this.radius = circle.radius;
+		this.shape = "circle";
 	}
 
-	public ShapeCalculatorV2(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public ShapeCalculatorV2(Rectangle rectangle) {
+		this.length = rectangle.length;
+		this.width = rectangle.width;
+		this.shape = "rectangle";
 	}
 
-	public double getPerimeter(int radius) {
-		this.radius = radius;
-		double perimeter = 2 * radius * Math.PI;
-		System.out.println("圓的周長是 " + perimeter);
-		return perimeter;
+	public void getPerimeter() {
+
+		if (this.shape.equals("circle")) {
+			double perimeter = 2 * radius * Math.PI;
+			System.out.println("圓的周長是 " + perimeter);
+		} else {
+			double perimeter = 2 * (length + width);
+			System.out.println("方形的周長是 " + perimeter);
+		}
 	}
 
-	public double getPerimeter(int x, int y) {
-		this.x = x;
-		this.y = y;
-		double perimeter = 2 * (x + y);
-		System.out.println("方形的周長是 " + perimeter);
-
-		return perimeter;
+	public void getArea() {
+		if (this.shape.equals("circle")) {
+			double cAare = radius * radius * Math.PI;
+			System.out.println("圓的面積是 " + cAare);
+		} else {
+			double tArea = length * width;
+			System.out.println("方形的面積是 " + tArea);
+		}
 	}
-
-	public double getArea(int radius) {
-		this.radius = radius;
-		double area = radius * radius * Math.PI;
-		System.out.println("圓的面積是 " + area);
-
-		return area;
-	}
-
-	public double getArea(int x, int y) {
-		this.x = x;
-		this.y = y;
-		double area = x * y;
-		System.out.println("方形的面積是 " + area);
-		return area;
-	}
-
 }
