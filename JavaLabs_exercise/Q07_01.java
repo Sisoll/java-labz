695c615dbbf79303c9e4b53120ff9e15f51caaf5
Sisol Shie <sisolshie@gmail.com>
2022-05-10 21:49:24 +0800

    modify: 修改sout內容以判斷程式碼執行順序

--- JavaLabs_exercise/Q07_01.java
+++ JavaLabs_exercise/Q07_01.java
@@ -7,23 +7,34 @@ package JavaLabs_exercise;
  *</pre>
  */
 public class Q07_01 {
+	static int  a;
 
-	{System.out.println("0");}
-    static {    System.out.println("A"); /* 此處為靜態區塊(static code block) A  */ } 
+	{System.out.println("區塊1");}
+	
+    static {    System.out.println("靜態區塊A"); /* 此處為靜態區塊(static code block) A  */ } 
+    
     Q07_01 () { System.out.println("建構子"); /* 此處為建構子  */ } 
-	{System.out.println("1");}
+    
+	{System.out.println("區塊2");}
 
-    static {     System.out.println("B");/* 此處為靜態區塊(static code block) B  */ } 
+    static {     System.out.println("靜態區塊B");/* 此處為靜態區塊(static code block) B  */ } 
+    
     public static void main(String args[]) { 
-        Q07_01 con = new Q07_01 (); 
-    	{System.out.println("1330");}
+    	
+    	System.out.println("main起點");
+    		// Q07_01 con = new Q07_01 (); 
+    	{System.out.println("main內的區塊");}
 
-        System.out.print("HaHaHa") ; 
-    	{System.out.println("444441330");}
+        System.out.print("main內的內容") ; 
+        
+    	{System.out.println("main內的區塊");}
     } 
-    static { System.out.println("C");/* 此處為靜態區塊(static code block) C  */ } 
-	{System.out.println("3");}
-
+    
+    static { System.out.println("靜態區塊C");/* 此處為靜態區塊(static code block) C  */ } 
+    
+	{System.out.println("在main後面的區塊3");}
+	
+	static{ System.out.println("靜態區塊" + a);}
 }
 
 
