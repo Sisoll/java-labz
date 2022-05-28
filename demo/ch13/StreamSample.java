6bf4cefb7707a0ab164dab04b44d4006d0932d5d
Bo-Xuan Fan <coffee0127@gmail.com>
2022-05-28 17:44:31 +0800

    Use IntStream#sum

--- demo/ch13/StreamSample.java
+++ demo/ch13/StreamSample.java
@@ -1,13 +1,11 @@
 package demo.ch13;
 
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class StreamSample {
     public static void main(String[] args) {
         // Step 1: 建立一個 List，內容為 1 ~ 10 的 int
-        Integer sum = IntStream.rangeClosed(1, 10)
-            .boxed()
+        int sum = IntStream.rangeClosed(1, 10)
             // Step 2: 將每個值 x 2 + 1
             .map(i -> i * 2 + 1)
             // Step 3: 保留是 3 的倍數
@@ -15,7 +13,7 @@ public class StreamSample {
             // Step 4: 將每個值變平方
             .map(i -> i * i)
             // Step 5: 計算總合
-            .collect(Collectors.summingInt(e -> e));
+            .sum();
 
         // Note: add `-ea` to VM arguments
         assert sum == 756;
