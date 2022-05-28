71ecd6a07be3aa85fc0193c9b84be67205014451
Bo-Xuan Fan <coffee0127@gmail.com>
2022-05-28 18:02:05 +0800

    把字串轉成 int 並回傳 NumberType

--- demo/ch13/OptionalSample.java
+++ demo/ch13/OptionalSample.java
@@ -1,13 +1,27 @@
 package demo.ch13;
 
+import java.util.Optional;
+
 public class OptionalSample {
     public static void main(String[] args) {
         // Note: add `-ea` to VM arguments
         assert getType(null) == NumberType.UNKNOWN;
+        assert getType("1") == NumberType.ODD;
+        assert getType("2") == NumberType.EVEN;
     }
 
     static NumberType getType(String str) {
-        return NumberType.UNKNOWN;
+        return Optional.ofNullable(str)
+            // 把字串轉成 int
+            .map(s -> Integer.parseInt(s))
+            .map(i -> {
+                // 檢查是奇數 / 偶數
+                if (i % 2 == 0) {
+                    return NumberType.EVEN;
+                }
+                return NumberType.ODD;
+            })
+            .orElse(NumberType.UNKNOWN);
     }
 
     enum NumberType {
